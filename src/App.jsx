eee38a5ee183c060299a0b6001862707c2b303dc
Jonas Leite <jonas.programador01@gmail.com>
2024-12-06 17:36:37 -0300

    :sparkles: feat: implement  css-modules in project starts with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/header";
 import { Post } from "./Post";
 
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Jonas o brabo"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos repudiandae nemo suscipit? Aut, quibusdam iusto consequuntur doloribus repellendus magnam eum odio quaerat quam possimus! Magnam nesciunt eius vel ab minima."
